ecb575c4a560247bbc6b3b46306e71f180d1dc0f
Wagner Lima <wagner.araujo92@outlook.com>
2022-12-11 19:54:55 -0300

    estrutura de projeto do modulo infraestrutura

---
+++ DominandoEFCore-Modulo3-Infraestrutura/Data/ApplicationContext.cs
@@ -0,0 +1,35 @@
+using DominandoEFCore_Modulo3_Infraestrutura.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace DominandoEFCore_Modulo3_Infraestrutura.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        private readonly StreamWriter _writer = new StreamWriter("meu_log_do_ef_core.txt", append: true);
+        public DbSet<Departamento> Departamentos { get; set; }
+        public DbSet<Funcionario> Funcionarios { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            const string strConnection="Data source=(localdb)\\mssqllocaldb; Initial Catalog=DevIO-02;Integrated Security=true;pooling=true;";
+
+            optionsBuilder
+                .UseSqlServer(
+                    strConnection, 
+                        o=> o
+                            .MaxBatchSize(100)
+                            .CommandTimeout(5)
+                            .EnableRetryOnFailure(4, TimeSpan.FromSeconds(10), null))
+                .LogTo(Console.WriteLine, LogLevel.Information)
+                .EnableSensitiveDataLogging()
+                ;
+        }
+
+        public override void Dispose()
+        {
+            base.Dispose();
+            _writer.Dispose();
+        }
+    }
+}
---
+++ DominandoEFCore-Modulo3-Infraestrutura/Program.cs
@@ -0,0 +1,78 @@
+﻿#nullable enable
+using DominandoEFCore_Modulo3_Infraestrutura.Data;
+using DominandoEFCore_Modulo3_Infraestrutura.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace DominandoEFCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TempoComandoGeral();
+            //HabilitandoBatchSize();
+            //DadosSensiveis();
+            //ConsultarDepartamentos();
+        }
+
+        static void ExecutarEstrategiaResiliencia()
+        {
+            using var db = new ApplicationContext();
+
+            var strategy = db.Database.CreateExecutionStrategy();
+            strategy.Execute(() =>
+            {
+                using var transaction = db.Database.BeginTransaction();
+
+                db.Departamentos.Add(new Departamento { Descricao = "Departamento Transacao" });
+                db.SaveChanges();
+
+                transaction.Commit();
+            });
+
+        }
+
+
+        static void TempoComandoGeral()
+        {
+            using var db = new ApplicationContext();
+
+            db.Database.SetCommandTimeout(10);
+
+            db.Database.ExecuteSqlRaw("WAITFOR DELAY '00:00:07';SELECT 1");
+        }
+
+        static void HabilitandoBatchSize()
+        {
+            using var db = new ApplicationContext();
+            db.Database.EnsureDeleted();
+            db.Database.EnsureCreated();
+
+            for (var i = 0; i < 50; i++)
+            {
+                db.Departamentos.Add(
+                    new Departamento
+                    {
+                        Descricao = "Departamento " + i
+                    });
+            }
+
+            db.SaveChanges();
+        }
+
+        static void DadosSensiveis()
+        {
+            using var db = new ApplicationContext();
+
+            var descricao = "Departamento";
+            var departamentos = db.Departamentos.Where(p => p.Descricao == descricao).ToArray();
+        }
+
+        static void ConsultarDepartamentos()
+        {
+            using var db = new ApplicationContext();
+
+            var departamentos = db.Departamentos.Where(p => p.Id > 0).ToArray();
+        }
+    }
+}
