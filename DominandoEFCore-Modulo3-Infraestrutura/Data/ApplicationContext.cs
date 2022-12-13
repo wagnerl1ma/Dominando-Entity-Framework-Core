1cd3c792f61bb6dc89827078c29a2c2056851db1
Wagner Lima <wagner.araujo92@outlook.com>
2022-12-13 20:11:06 -0300

    Gravando logs em um txt

--- DominandoEFCore-Modulo3-Infraestrutura/Data/ApplicationContext.cs
+++ DominandoEFCore-Modulo3-Infraestrutura/Data/ApplicationContext.cs
@@ -7,7 +7,9 @@ namespace DominandoEFCore_Modulo3_Infraestrutura.Data
 {
     public class ApplicationContext : DbContext
     {
-        private readonly StreamWriter _writer = new StreamWriter("meu_log_do_ef_core.txt", append: true);
+        //static string? caminho = Path.GetDirectoryName(Directory.GetCurrentDirectory());
+        static string? caminho = "C:\\Users\\Wagner\\Desktop\\DESENVOLVEDOR.IO\\ENTITY FRAMEWORK CORE\\Dominando o Entity Framework Core\\Projeto\\DominandoEFCore\\DominandoEFCore-Modulo3-Infraestrutura";
+        private readonly StreamWriter _writer = new StreamWriter($"{caminho}\\meu_log_do_ef_core.txt", append: true); // append: acrescenta todo vez mais informaçoes no mesmo arquivo
         public DbSet<Departamento> Departamentos { get; set; }
         public DbSet<Funcionario> Funcionarios { get; set; }
 
@@ -17,10 +19,11 @@ namespace DominandoEFCore_Modulo3_Infraestrutura.Data
 
             optionsBuilder
                 .UseSqlServer(strConnection, o => o.MaxBatchSize(100).CommandTimeout(5).EnableRetryOnFailure(4, TimeSpan.FromSeconds(10), null))
-                //.LogTo(Console.WriteLine, LogLevel.Information) //inserindo log no console
-                .LogTo(Console.WriteLine, new[] { CoreEventId.ContextInitialized, RelationalEventId.CommandExecuted},
+                //.LogTo(Console.WriteLine, LogLevel.Information); //inserindo log no console
+                .LogTo(_writer.WriteLine, LogLevel.Information); //Gravando logs em um arquivo de texto
+                /*.LogTo(Console.WriteLine, new[] { CoreEventId.ContextInitialized, RelationalEventId.CommandExecuted},
                                                   LogLevel.Information, DbContextLoggerOptions.LocalTime | DbContextLoggerOptions.SingleLine) //filtrando eventos do log
-                .EnableSensitiveDataLogging();
+                .EnableSensitiveDataLogging();*/
         }
 
         public override void Dispose()
