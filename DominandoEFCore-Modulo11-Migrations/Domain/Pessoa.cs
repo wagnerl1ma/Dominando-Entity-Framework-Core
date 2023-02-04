f2058f4f1228268a0bab16ff23f10a92c2c5502b
Wagner Lima <wagner.araujo92@outlook.com>
2023-02-04 11:48:53 -0300

    Estrutura do modulo Migrations

---
+++ DominandoEFCore-Modulo11-Migrations/Domain/Pessoa.cs
@@ -0,0 +1,9 @@
+namespace DominandoEFCore_Modulo11_Migrations.Domain
+{
+    public class Pessoa
+    {
+        public int Id { get; set; }
+        public int Nome { get; set; }
+        public string Telefone { get; set; }
+    }
+}
---
+++ DominandoEFCore-Modulo11-Migrations/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using DominandoEFCore_Modulo11_Migrations.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace DominandoEFCore_Modulo11_Migrations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using var db = new ApplicationContext();
+
+            //db.Database.Migrate();
+
+            var migracoes = db.Database.GetPendingMigrations();
+            foreach (var migracao in migracoes)
+            {
+                Console.WriteLine(migracao);
+            }
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
