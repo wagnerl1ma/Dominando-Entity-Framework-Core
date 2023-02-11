f8d7be4df07dfa7747a88f8061b21bbf421040fd
Wagner Lima <wagner.araujo92@outlook.com>
2023-02-11 12:28:31 -0300

    estrutura modulo Banco de Dados

---
+++ DominandoEFCore-Modulo12-BancoDeDados/Domain/Pessoa.cs
@@ -0,0 +1,9 @@
+namespace DominandoEFCore_Modulo12_BancoDeDados.Domain
+{
+    public class Pessoa
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+    }
+}
---
+++ DominandoEFCore-Modulo12-BancoDeDados/Program.cs
@@ -0,0 +1,31 @@
+﻿using DominandoEFCore_Modulo12_BancoDeDados.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace DominandoEFCore_Modulo12_BancoDeDados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using var db = new ApplicationContext();
+
+            db.Database.EnsureCreated();
+
+            db.Pessoas.Add(new DominandoEFCore_Modulo12_BancoDeDados.Domain.Pessoa
+            {
+                Id = 1,
+                Nome = "TESTE",
+                Telefone = "999999"
+            });
+
+            db.SaveChanges();
+
+            var pessoas = db.Pessoas.ToList();
+            foreach (var item in pessoas)
+            {
+                Console.WriteLine($"Nome: {item.Nome}");
+            }
+
+        }
+    }
+}
